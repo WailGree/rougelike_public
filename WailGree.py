ae10c83206eb0d540eb5a7df6d8af1e89625ac16
WailGree <szilagyi.roland.dominik@gmail.com>
2020-06-18 14:59:23 +0200

    fixed story

--- WailGree.py
+++ WailGree.py
@@ -28,24 +28,26 @@ def write_message(speaker, message):
 
 def story_telling():
     write_message("Stranger", "Hey! You! You're finally awake.")
-    input('Press ENTER to continue...')
+    input('\nPress ENTER to continue...')
     write_message('You', "W-what? Who are you? Where am I?")
-    input('Press ENTER to continue...')
+    input('\nPress ENTER to continue...')
     write_message(
         'Stranger', "I am... I was a fellow student like you... We are in the basement of Codecool.")
-    input('Press ENTER to continue...')
+    input('\nPress ENTER to continue...')
     write_message('You', 'In where?!?! How did I get here? Last time I remember... It was 3rd day of TW week. I was about to leave.\nThen I got hit in the back of my head...')
-    input('Press ENTER to continue...')
+    input('\nPress ENTER to continue...')
     write_message(
         'Student', 'Everyone who doesn\'t study ends up here... This is why there are news about students who suddenly disappear.')
+    input('\nPress ENTER to continue...')
     write_message('You', 'No! That can\'t be!! I HAVE TO GET OUT OF HERE!!!!')
-    input('Press ENTER to continue...')
+    input('\nPress ENTER to continue...')
     write_message('Student', 'There is a way... The basement holds creatures, unknown to us. Nobody who passed the first room ever returned.\n You have to defeat what\'s to come, to leave the basement.')
-    input('Press ENTER to continue...')
+    input('\nPress ENTER to continue...')
     write_message('You', 'I have to try!')
-    input('Press ENTER to continue...')
+    input('\nPress ENTER to continue...')
     write_message(
         'Student', 'Then I\'ll wish you the best of luck... But before you go, I have a few questions...')
+    input('\nPress ENTER to continue...')
 
 
 def combat_display(creature1, creature2):
